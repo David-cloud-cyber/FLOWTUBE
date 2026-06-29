ada7e5e39bb729f96c7cfc89e8de2a3f8887e5d7
David-cloud-cyber <novacore629@gmail.com>
2026-06-29 23:21:34 +0200

    Fit production API routes within Vercel Hobby limits

--- api/auth/[action].js
+++ api/auth/[action].js
@@ -1,5 +1,5 @@
 const { proxy } = require("../_proxy.js");
 
 module.exports = async function handler(req, res) {
-  return proxy(req, res, `/generations/${encodeURIComponent(req.query.id)}`);
+  return proxy(req, res, `/auth/${encodeURIComponent(req.query.action)}`);
 };
